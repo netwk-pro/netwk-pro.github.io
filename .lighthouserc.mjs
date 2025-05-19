d752bfbf12759982105c506118327349474ec550
SunDevil311 <github@sl.neteng.cc>
2025-05-19 03:16:33 -0700

    Corrected Lighthouse config

---
+++ .lighthouserc.mjs
@@ -0,0 +1,23 @@
+/* ==========================================================================
+.lighthouserc.mjs
+
+SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+This file is part of Network Pro.
+========================================================================== */
+
+/** @type {import('lighthouse').Config} */
+const config = {
+  ci: {
+    collect: {
+      url: ["https://netwk.pro"],
+      settings: {
+        onlyCategories: ["pwa"],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
+
+export default config;
