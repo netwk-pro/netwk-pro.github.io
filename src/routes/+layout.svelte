2cd9617d2bb46510833c855fecf738ecc42a196c
SunDevil311 <github@sl.neteng.cc>
2025-05-18 18:57:17 -0700

    Comment inadvertently removed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,10 @@
+<!-- ==========================================================================
+src/routes/+layout.svelte.svelte
+
+SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+This file is part of Network Pro.
+========================================================================== -->
+
 <script>
   export let data;
 
