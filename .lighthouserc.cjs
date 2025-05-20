0bfe1b0560e2ed2bbced09c5025bba363cd3acbc
SunDevil311 <github@sl.neteng.cc>
2025-05-20 00:17:28 -0700

    Re-added assert block

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -17,6 +17,27 @@ const config = {
     upload: {
       target: "temporary-public-storage",
     },
+    assert: {
+      assertions: {
+        "service-worker": ["error", { minScore: 1 }],
+        "installable-manifest": ["error", { minScore: 1 }],
+        "maskable-icon": "off",
+        "splash-screen": "off",
+        viewport: "off",
+        "content-width": "off",
+        "themed-omnibox": "off",
+        // Disable general categories you don't care about
+        "errors-in-console": "off",
+        "inspector-issues": "off",
+        "link-in-text-block": "off",
+        "third-party-cookies": "off",
+        "target-size": "off",
+        "first-contentful-paint": "off",
+        interactive: "off",
+        "largest-contentful-paint": "off",
+        "uses-long-cache-ttl": "off",
+      },
+    },
     output: ["html"],
     outputPath: ".lighthouseci",
   },
