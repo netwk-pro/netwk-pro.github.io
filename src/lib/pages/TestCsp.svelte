9940f36013fb08532560d0347b7873cef34aca76
SunDevil311 <github@sl.neteng.cc>
2025-06-01 02:52:02 -0700

    Updated test-csp/ route so that it will properly trigger a violation

---
+++ src/lib/pages/TestCsp.svelte
@@ -0,0 +1,17 @@
+<!-- ==========================================================================
+src/lib/pages/TestCsp.svelte
+
+Copyright © 2025 Network Pro Strategies (Network Pro™)
+SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+This file is part of Network Pro.
+========================================================================== -->
+
+<h1>CSP Violation Test</h1>
+
+<iframe src="https://example.com" width="600" height="400" title="Test iframe"
+></iframe>
+
+<p style="font-weight: bold; color: red; align-content: center;">
+  ⚠️ This page intentionally violates the site's Content Security Policy for
+  testing purposes.
+</p>
--- src/routes/test-csp/+page.svelte
+++ src/routes/test-csp/+page.svelte
@@ -1,16 +1,22 @@
-<script>
-  import { onMount } from "svelte";
+<!-- ==========================================================================
+src/routes/test-csp/+page.svelte
 
-  onMount(() => {
-    const script = document.createElement("script");
-    script.src = "https://evil.example.com/nonExistent.js";
-    document.body.appendChild(script);
-  });
+Copyright © 2025 Network Pro Strategies (Network Pro™)
+SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+This file is part of Network Pro.
+========================================================================== -->
+
+<script>
+  import FullWidthSection from "$lib/components/FullWidthSection.svelte";
+  import TestCsp from "$lib/pages/TestCsp.svelte";
 </script>
 
-<h1>CSP Violation Test</h1>
-
-<p style="font-weight: bold; color: red;">
-  ⚠️ This page intentionally violates the site's Content Security Policy for
-  testing purposes.
-</p>
+<section id="test-csp">
+  <FullWidthSection>
+    <!-- BEGIN TEST CSP PAGE -->
+    <section id="main-content">
+      <TestCsp />
+    </section>
+    <!-- END TEST CSP PAGE -->
+  </FullWidthSection>
+</section>
