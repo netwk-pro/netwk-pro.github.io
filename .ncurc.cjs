887be8a2b47fc996e4406b15d890f670a008be2f
Scott Lopez <github@sl.neteng.cc>
2025-11-16 05:25:21 -0700

    chore: release prep for v1.25.13 (#164)

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -31,13 +31,7 @@ This file is part of Network Pro.
 /** @type {import('npm-check-updates').RunOptions} */
 module.exports = {
   // Ignore specific dependencies (prevent upgrades)
-  reject: [
-    'vitest',
-    '@vitest/coverage-v8',
-    'prettier',
-    'jsdom',
-    'markdownlint-cli2',
-  ],
+  reject: ['vitest', '@vitest/coverage-v8', 'prettier', 'jsdom'],
 
   // Always upgrade devDependencies as well
   dep: 'prod,dev',
