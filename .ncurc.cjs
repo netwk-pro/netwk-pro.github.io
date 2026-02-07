bf1240e3230f7f33ea11a9cdd9b157145c08c6d2
Scott Lopez <github@sl.neteng.cc>
2026-02-07 00:46:28 -0700

    chore: release prep for v1.26.8 (#197)
    * chore: release prep for v1.26.8
    
    * docs: updated changelog

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -31,7 +31,7 @@ This file is part of Network Pro.
 /** @type {import('npm-check-updates').RunOptions} */
 module.exports = {
   // Ignore specific dependencies (prevent upgrades)
-  reject: ['prettier'],
+  reject: ['eslint', '@eslint/js'],
 
   // Always upgrade devDependencies as well
   dep: 'prod,dev',
